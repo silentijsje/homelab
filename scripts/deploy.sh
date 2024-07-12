91941f6150cb761b9d542802b6cb53811a6b5ce9
silentijsje <github@silentijsje>
2024-07-12 09:11:55 +0200

    Refactor mountShares.yml to use consistent state for mounting network shares

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,6 @@
 cd /home/gamer0308/github/homelab
 git pull
 build_version=$(git rev-parse --short HEAD)
-echo "$(TZ='Europe/Amsterdam' date +%FT%T%z): Releasing new server version. $build_version" >> /mnt/backup/code/github/logs/prod-code02.log
+# echo "$(TZ='Europe/Amsterdam' date +%FT%T%z): Releasing new server version. $build_version" >> /mnt/backup/code/github/logs/prod-code02.log
 echo "$(TZ='Europe/Amsterdam' date +%FT%T%z): Running build...."
 ./install.sh
