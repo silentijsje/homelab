55e03e4fbb86f0f4812df7b18a737f44b99d5472
silentijsje <github@silentijsje.com>
2024-08-15 16:07:43 +0200

    Refactor docker shared folder paths and update container configurations

--- decrypt.sh
+++ decrypt.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 VAULT_FILE=./ansible/vars/vault.yml
+IMMICH_ENV_FILE=./ansible/files/prod-video01/immich/env
 VAULT_PASSWORD_FILE=./ansible/.vault_pass
 
 # Decrypt the Ansible Vault file
 ansible-vault decrypt "$VAULT_FILE" --vault-password-file "$VAULT_PASSWORD_FILE"
+ansible-vault decrypt "$IMMICH_ENV_FILE" --vault-password-file "$VAULT_PASSWORD_FILE"
 
 if [ $? -eq 0 ]; then
     echo "Vault file $VAULT_FILE decrypted successfully."
--- encrypt.sh
+++ encrypt.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 VAULT_FILE=./ansible/vars/vault.yml
+IMMICH_ENV_FILE=./ansible/files/prod-video01/immich/env
 VAULT_PASSWORD_FILE=./ansible/.vault_pass
 
 # Encrypt the file into an Ansible Vault file
 ansible-vault encrypt "$VAULT_FILE" --vault-password-file "$VAULT_PASSWORD_FILE"
+ansible-vault encrypt "$IMMICH_ENV_FILE" --vault-password-file "$VAULT_PASSWORD_FILE"
 
 if [ $? -eq 0 ]; then
     echo "File $VAULT_FILE encrypted successfully."
