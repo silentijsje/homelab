563e8c27529171a85b5ea48125edecd62bf9cc05
silentijsje <github@silentijsje.com>
2024-08-18 14:16:31 +0200

    Refactor docker shared folder paths and update container configurations

---
+++ scripts/docker-backup.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+APPDATA_LOC="/mnt/docker"
+BACKUP_LOC="/mnt/backup/docker"
+COMPOSE_LOC="/mnt/ansible/docker-compose.yml"
+
+date=$(date -I)
+hostname=$(hostname -s)
+archive="$(date '+%Y-%m-%d')"
+
+
+function update {
+    echo "Searching for yq"
+    if which yq; then
+        echo "yq found, continuing"
+    else
+        echo "Please install yq first"
+        exit 1
+    fi
+
+    sudo docker compose -f "$COMPOSE_LOC" pull
+    docker compose -f "$COMPOSE_LOC" down
+
+    echo "Starting backup"
+    mkdir -p "$BACKUP_LOC/$hostname/$archive"
+    cp -a "$COMPOSE_LOC" $BACKUP_LOC/$hostname/$archive/docker-compose.yml.bak
+    sudo tar -czf $BACKUP_LOC/$hostname/$archive/appdatabackup.tar.gz $APPDATA_LOC
+
+    docker compose -f "$COMPOSE_LOC" up -d
+    sudo chown "${USER}":"${USER}" -R $BACKUP_LOC/$hostname/$archive/appdatabackup.tar.gz
+
+    docker image prune -f
+
+    echo "Backup finished"
+}
+
+function restore {
+    sudo docker compose -f "$COMPOSE_LOC" down
+    randstr=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-8};echo;)
+    sudo mv "$APPDATA_LOC" "${APPDATA_LOC}.$randstr"
+    sudo cp -a "$COMPOSE_LOC" "${COMPOSE_LOC}.$randstr"
+    mkdir -p "$APPDATA_LOC"
+    sudo tar xvf $BACKUP_LOC/$hostname/$archive/appdatabackup.tar.gz $APPDATA_LOC
+    docker compose -f "$COMPOSE_LOC" pull
+    docker compose -f "$COMPOSE_LOC" up -d
+}
+
+# Check if the function exists
+if declare -f "$1" > /dev/null; then
+  "$@"
+else
+  echo "The only valid arguments are update, restore, and resume"
+  exit 1
+fi
