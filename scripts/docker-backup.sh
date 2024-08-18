f251542873c10fd1865adfb342e5c8934cc600bb
silentijsje <github@silentijsje.com>
2024-08-18 16:46:55 +0200

    Refactor docker shared folder paths and update container configurations

--- scripts/docker-backup.sh
+++ scripts/docker-backup.sh
@@ -10,44 +10,46 @@ archive="$(date '+%Y-%m-%d')"
 
 
 function update {
-    echo "Searching for yq"
-    if which yq; then
-        echo "yq found, continuing"
-    else
-        echo "Please install yq first"
-        exit 1
-    fi
+  echo "Searching for yq"
+  if which yq; then
+    echo "yq found, continuing"
+  else
+    echo "Please install yq first"
+    exit 1
+  fi
 
-    sudo docker compose -f "$COMPOSE_LOC" pull
-    docker compose -f "$COMPOSE_LOC" down
+  sudo docker compose -f "$COMPOSE_LOC" pull
+  docker compose -f "$COMPOSE_LOC" down
 
-    echo "Starting backup"
-    mkdir -p "$BACKUP_LOC/$hostname/$archive" "$BACKUP_LOC/$hostname/latest"
-    cp -a "$COMPOSE_LOC" $BACKUP_LOC/$hostname/$archive/docker-compose.yml.bak
-    sudo tar -czf $BACKUP_LOC/$hostname/$archive/appdatabackup.tar.gz $APPDATA_LOC
+  echo "Starting backup"
+  APPDATA_NAME=$(echo "$APPDATA_LOC" | awk -F/ '{print $NF}')
+  mkdir -p "$BACKUP_LOC/$hostname/$archive" "$BACKUP_LOC/$hostname/latest"
 
-    cp -a "$COMPOSE_LOC" $BACKUP_LOC/$hostname/$archive/docker-compose.yml.bak
-    sudo tar -czf $BACKUP_LOC/$hostname/latest/appdatabackup.tar.gz $APPDATA_LOC
+  cp -a "$COMPOSE_LOC" $BACKUP_LOC/$hostname/latest/docker-compose.yml.bak
+  sudo tar -C "$APPDATA_LOC"/.. -czf $BACKUP_LOC/$hostname/latest/appdatabackup.tar.gz "$APPDATA_NAME"
+  cp -a $BACKUP_LOC/$hostname/latest/* $BACKUP_LOC/$hostname/$archive/
 
-    docker compose -f "$COMPOSE_LOC" up -d
-    sudo chown "${USER}":"${USER}" -R $BACKUP_LOC/$hostname/$archive/appdatabackup.tar.gz
+  # cp -a "$COMPOSE_LOC" $BACKUP_LOC/$hostname/$archive/docker-compose.yml.bak
+  # sudo tar -C "$APPDATA_LOC"/.. -czf $BACKUP_LOC/$hostname/$archive/appdatabackup.tar.gz "$APPDATA_NAME"
 
-    docker image prune -f
+  docker compose -f "$COMPOSE_LOC" up -d
+  sudo chown "${USER}":"${USER}" -R $BACKUP_LOC/$hostname/
 
-    echo "Backup finished"
+  docker image prune -f
 }
 
 function restore {
-    sudo docker compose -f "$COMPOSE_LOC" down
-    randstr=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-8};echo;)
-    sudo mv "$APPDATA_LOC" "${APPDATA_LOC}.$randstr"
-    sudo cp -a "$COMPOSE_LOC" "${COMPOSE_LOC}.$randstr"
-    sudo mkdir -p "$APPDATA_LOC"
-    sudo tar -xvf $BACKUP_LOC/$hostname/latest/appdatabackup.tar.gz $APPDATA_LOC
-    sudo chown "${USER}":"${USER}" -R "$APPDATA_LOC"
-    sudo chmod 770 -R "$APPDATA_LOC"
-    docker compose -f "$COMPOSE_LOC" pull
-    docker compose -f "$COMPOSE_LOC" up -d
+  sudo docker compose -f "$COMPOSE_LOC" down
+  randstr=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-8};echo;)
+  sudo mv "$APPDATA_LOC" "${APPDATA_LOC}.$randstr"
+  sudo cp -a "$COMPOSE_LOC" "${COMPOSE_LOC}.$randstr"
+  sudo mkdir -p "$APPDATA_LOC"
+  cp -a $BACKUP_LOC/$hostname/latest/docker-compose.yml.bak "$COMPOSE_LOC"
+  sudo tar -xf $BACKUP_LOC/$hostname/latest/appdatabackup.tar.gz -C "$APPDATA_LOC"/../
+  sudo chown "${USER}":"${USER}" -R "$APPDATA_LOC"
+  sudo chmod 770 -R "$APPDATA_LOC"
+  docker compose -f "$COMPOSE_LOC" pull
+  docker compose -f "$COMPOSE_LOC" up -d
 }
 
 # Check if the function exists
