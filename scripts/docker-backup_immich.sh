d2d28beea3bf2d7a3962a125ee14e44bfd3779a9
silentijsje <github@silentijsje.com>
2024-09-30 13:55:44 +0200

    Refactor ansible tasks to copy scripts and create cronjob for docker backup

---
+++ scripts/docker-backup_immich.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+APPDATA_LOC="/mnt/docker"
+BACKUP_LOC="/mnt/backup/docker"
+
+date=$(date -I)
+hostname=$(hostname -s)
+archive="$(date '+%Y-%m-%d')"
+
+
+function update {
+  sudo docker compose -f "$COMPOSE_LOC" pull
+  docker compose -f "$COMPOSE_LOC" down
+
+  echo "Starting backup"
+  APPDATA_NAME=$(echo "$APPDATA_LOC" | awk -F/ '{print $NF}')
+  mkdir -p "$BACKUP_LOC/$hostname/$archive" "$BACKUP_LOC/$hostname/latest"
+
+  sudo tar -C "$APPDATA_LOC"/.. -czf $BACKUP_LOC/$hostname/latest/appdatabackup.tar.gz "$APPDATA_NAME"
+  cp -a $BACKUP_LOC/$hostname/latest/* $BACKUP_LOC/$hostname/$archive/
+
+  docker compose -f "$COMPOSE_LOC" up -d
+  sudo chown "${USER}":"${USER}" -R $BACKUP_LOC/$hostname/
+
+  docker image prune -f
+  cleanup_old_backups
+}
+
+function cleanup_old_backups {
+  echo "Cleaning up old backups"
+  backup_dirs=($(ls -dt $BACKUP_LOC/$hostname/*/ | grep -v "latest"))
+  if [ ${#backup_dirs[@]} -gt 10 ]; then
+    dirs_to_delete=(${backup_dirs[@]:10})
+    for dir in "${dirs_to_delete[@]}"; do
+      echo "Deleting old backup: $dir"
+      rm -rf "$dir"
+    done
+  fi
+}
+
+function restore {
+  sudo docker compose -f "$COMPOSE_LOC" down
+  randstr=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-8};echo;)
+  sudo mv "$APPDATA_LOC" "${APPDATA_LOC}.$randstr"
+  sudo cp -a "$COMPOSE_LOC" "${COMPOSE_LOC}.$randstr"
+  sudo mkdir -p "$APPDATA_LOC"
+  cp -a $BACKUP_LOC/$hostname/latest/docker-compose.yml.bak "$COMPOSE_LOC"
+  sudo tar -xf $BACKUP_LOC/$hostname/latest/appdatabackup.tar.gz -C "$APPDATA_LOC"/../
+  sudo chown "${USER}":"${USER}" -R "$APPDATA_LOC"
+  sudo chmod 770 -R "$APPDATA_LOC"
+  docker compose -f "$COMPOSE_LOC" pull
+  docker compose -f "$COMPOSE_LOC" up -d
+}
+
+# Check if the function exists
+if declare -f "$1" > /dev/null; then
+  "$@"
+else
+  echo "The only valid arguments are update, restore"
+  exit 1
+fi
